a9f0089c381dae3cec1563460393765c73294e09
Cooper Rictor <cooperrictor@ad.weber.edu>
2019-08-05 10:13:42 -0600

    Initial commit

---
+++ numbers.py
@@ -0,0 +1,35 @@
+#print("Hello Python")
+
+# scalar data types: Integers, Floating point
+# None, and Boolean
+
+print("Binary notation", 0b0100)
+print("Hex notation", 0x0100)
+
+# use the int() constructor
+
+print("477")
+
+# option for base
+
+print(int("1001", 2))
+
+# Floating point: IEE-754 notation
+
+# Scientific notation
+
+print("scientific notation", 1.61e-35)
+print("Float construction float()", float("1.99"))
+
+# Boolean
+
+print("Boolean constructor bool()", bool(0), bool(1))
+print("Boolean constructor bool(42)", bool(42))
+print("Boolean constructor bool(0.1)", bool(.01))
+
+# test boolean on collection
+
+print("Boolean constructor bool([])", bool([]))
+print("Boolean constructor bool([1,2,3])", bool([1, 2, 3]))
+print("Boolean constructor bool("")", bool(""))
+print("Boolean constructor bool('false')", bool("false"))
