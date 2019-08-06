356d8f67f6d198f0fc7fca42a2de2605ed4bd1a1
cooperrictor <crictor@outlook.com>
2019-08-06 10:32:38 -0600

    rename files in a folder

---
+++ file_rename.py
@@ -0,0 +1,41 @@
+"""
+Rename files from a folder
+get: http://icarus.cs.weber.edu/~hvalle/hafb/prank.zip
+"""
+
+import os
+
+
+
+
+def rename_files():
+    """
+    Rename files in a folder
+    :return: nothing
+    """
+    folder_dir = r"C:\Users\cooperrictor.AD\Desktop\Hafb\prank_backup"
+    files = os.listdir(folder_dir)
+    saved_path = os.getcwd()                #saves current working directory
+    #change directory to the files directory
+    os.chdir(folder_dir)
+    for file in files:
+        #remove digits from name
+        new_file = file.lstrip('0123456789')
+        print("Old name", file, "New name", new_file)
+        #rename file to new name
+        os.renames(file, new_file)
+    #get back to home directory
+    os.chdir(saved_path)
+
+
+def main():
+    """
+    test function
+    :return: nothing
+    """
+    rename_files()
+
+
+if __name__ == '__main__':
+    main()
+    exit(0)
