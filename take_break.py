ad58bcb821e9da6b3e7d042e0ac5315a7f7249c3
cooperrictor <crictor@outlook.com>
2019-08-06 09:43:54 -0600

    plays video every hour for a break

---
+++ take_break.py
@@ -0,0 +1,25 @@
+"""
+Set an alarm to take a break by playing a youtube video
+"""
+
+import webbrowser
+import time
+
+def main():
+    """
+    Test function
+    :return: nothing
+    """
+
+    video_address = "https://www.youtube.com/watch?v=0GgYLdf1ago"
+
+    counter = 0
+    while counter < 3:
+        time.sleep(60*60)   #delay video 1 hour
+        webbrowser.open(video_address)
+        counter += 1
+
+
+if __name__ == '__main__':
+    main()
+    exit(0)
